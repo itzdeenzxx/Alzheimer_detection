d8e2a6c94fead0513c4a36d02886aea68eb0104a
itzdeenzxx <fruitdf123@gmail.com>
2024-03-03 14:02:54 +0700

    success count

--- camera.py
+++ camera.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 import mediapipe as mp
 from class_fitness.L_pose import Hand_L_Detector,confirm_right,confirm_left,count_right,count_left
-from class_fitness.L_pose_2 import Hand_L_Detector_2,confirm_right_2,confirm_left_2,count_right_2,count_left_2
 
 mp_hands = mp.solutions.hands
 
@@ -12,10 +11,7 @@ class VideoCamera(object):
     def __init__(self):
         self.camera_index = 0 
         self.video = cv2.VideoCapture(self.camera_index)
-
         self.L_pose = Hand_L_Detector() #######
-        self.L_pose_2 = Hand_L_Detector_2() #######
-        self.swap_hand = 0
 
     def __del__(self):
         self.video.release()
@@ -25,24 +21,9 @@ class VideoCamera(object):
         ret, frame = self.video.read()
         if not ret:
             return None
-        cv2.putText(frame, str(cont_of_L_pose), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        if self.swap_hand >= 10 :
-            pass
-        elif self.swap_hand % 2 == 0:
-            self.L_pose.detect_and_count_finger_distance(frame)
-            if(confirm_left == 1 and confirm_right == 1):
-                print("1231231212121")
-                # self.count_hand_L()
-                cont_of_L_pose += 1
-                self.swap_hand += 1
-                self.L_pose.set_zero()
-
-        elif self.swap_hand % 2 == 1:
-            self.L_pose_2.detect_and_count_finger_distance_2(frame)
-            self.L_pose_2.set_zero()
-            self.count_hand_L()
-            self.swap_hand += 1
-
+        
+        self.L_pose.detect_and_count_finger_distance(frame)
+       
         ret, jpeg = cv2.imencode('.jpg', frame)
         if not ret:
             return None
--- class_fitness/L_pose.py
+++ class_fitness/L_pose.py
@@ -8,7 +8,7 @@ confirm_right = 0
 confirm_left = 0
 count_right = 0
 count_left = 0
-
+count_final = 0
 
 class Hand_L_Detector:
     def __init__(self):
@@ -19,41 +19,85 @@ class Hand_L_Detector:
         return math.sqrt((lm1.x - lm2.x)**2 + (lm1.y - lm2.y)**2)
 
     def detect_and_count_finger_distance(self, frame):
-        global confirm_right , confirm_left , count_right , count_left
+        global confirm_right , confirm_left , count_right , count_left , count_final
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results_hands = self.hands.process(rgb_frame)
 
+        cv2.putText(frame, str(count_final), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
         if results_hands.multi_hand_landmarks:
-            for hand_landmarks_inner, handedness_inner in zip(results_hands.multi_hand_landmarks, results_hands.multi_handedness):
-                label = MessageToDict(handedness_inner)['classification'][0]['label']
+            if confirm_left == 0 or confirm_right == 0 :
+                for hand_landmarks_inner, handedness_inner in zip(results_hands.multi_hand_landmarks, results_hands.multi_handedness):
+                    label = MessageToDict(handedness_inner)['classification'][0]['label']
 
-                thumb_tip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.THUMB_TIP]
-                index_tip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
-                middle_tip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
-                ring_tip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.RING_FINGER_TIP]
-                pinky_tip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.PINKY_TIP]
-                thumb_ip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.THUMB_IP]
-                index_dip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP]
-                middle_dip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_DIP]
-                ring_dip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.RING_FINGER_DIP]
-                pinky_dip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.PINKY_DIP]
+                    thumb_tip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.THUMB_TIP]
+                    index_tip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                    middle_tip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
+                    ring_tip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.RING_FINGER_TIP]
+                    pinky_tip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.PINKY_TIP]
+                    thumb_ip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.THUMB_IP]
+                    index_dip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP]
+                    middle_dip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_DIP]
+                    ring_dip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.RING_FINGER_DIP]
+                    pinky_dip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.PINKY_DIP]
 
-                if label == 'Right':
-                    if thumb_tip.x < thumb_ip.x and index_tip.y > index_dip.y and middle_tip.y < middle_dip.y and ring_tip.y < ring_dip.y and pinky_tip.y < pinky_dip.y:
-                        cv2.putText(frame, "Right great", (10, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                        confirm_right += 1
-                        if confirm_right == 1 :
-                            count_right += 1
+                    if label == 'Right':
+                        if thumb_tip.x < thumb_ip.x and index_tip.y > index_dip.y and middle_tip.y < middle_dip.y and ring_tip.y < ring_dip.y and pinky_tip.y < pinky_dip.y:
+                            cv2.putText(frame, "Right great", (10, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                            confirm_right += 1
+                            if confirm_right == 1 :
+                                count_right += 1
 
-                elif label == 'Left':
-                    if thumb_tip.x > thumb_ip.x and index_tip.y < index_dip.y and middle_tip.y > middle_dip.y and ring_tip.y > ring_dip.y and pinky_tip.y > pinky_dip.y:
-                        cv2.putText(frame, "Left great", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                        confirm_left += 1
-                        if confirm_left == 1 :
-                            count_left += 1
+                    if label == 'Left':
+                        if thumb_tip.x > thumb_ip.x and index_tip.y < index_dip.y and middle_tip.y > middle_dip.y and ring_tip.y > ring_dip.y and pinky_tip.y > pinky_dip.y:
+                            cv2.putText(frame, "Left great", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                            confirm_left += 1
+                            if confirm_left == 1 :
+                                count_left += 1
+                if count_left == count_right:
+                    count_final = count_left
 
                 mp.solutions.drawing_utils.draw_landmarks(frame, hand_landmarks_inner, mp_hands.HAND_CONNECTIONS)
-    
-    def set_zero(self) :
-        global confirm_right , confirm_left
-        confirm_right , confirm_left = 0,0
+            
+            elif confirm_left >= 1 and confirm_right >= 1 :
+                for hand_landmarks_inner, handedness_inner in zip(results_hands.multi_hand_landmarks, results_hands.multi_handedness):
+                    label = MessageToDict(handedness_inner)['classification'][0]['label']
+
+                    thumb_tip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.THUMB_TIP]
+                    index_tip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                    middle_tip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
+                    ring_tip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.RING_FINGER_TIP]
+                    pinky_tip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.PINKY_TIP]
+                    thumb_ip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.THUMB_IP]
+                    index_dip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP]
+                    middle_dip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_DIP]
+                    ring_dip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.RING_FINGER_DIP]
+                    pinky_dip = hand_landmarks_inner.landmark[mp_hands.HandLandmark.PINKY_DIP]
+
+                    
+
+                    if label == 'Left':
+                        if thumb_tip.x > thumb_ip.x and index_tip.y > index_dip.y and middle_tip.y < middle_dip.y and ring_tip.y < ring_dip.y and pinky_tip.y < pinky_dip.y:
+                            cv2.putText(frame, "Left great", (10, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                            confirm_right += 1
+                            if confirm_right >= 1 and count_right % 2 == 1:
+                                count_right += 1
+                                print(count_right)
+
+                    if label == 'Right':
+                        if thumb_tip.x < thumb_ip.x and index_tip.y < index_dip.y and middle_tip.y > middle_dip.y and ring_tip.y > ring_dip.y and pinky_tip.y > pinky_dip.y:
+                            cv2.putText(frame, "Right great", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                            confirm_left += 1
+                            if confirm_left >= 1 and count_left % 2 == 1 :
+                                count_left += 1
+                                print(count_left)
+
+                    if count_left == count_right and count_right % 2 == 0 and count_left % 2 == 0:
+                        count_final = count_left
+                        confirm_right , confirm_left = 0,0
+
+                    mp.solutions.drawing_utils.draw_landmarks(frame, hand_landmarks_inner, mp_hands.HAND_CONNECTIONS)
+            
+                
+
+
