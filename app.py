2bdb425d87221d8d41db53d6c3c4276d6a343e83
IsNName <wasawat.boonporn@gmail.com>
2024-03-10 21:45:36 +0700

    Numfinger

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
-from flask import Flask, render_template, Response , request
+from flask import Flask, render_template, Response, request
 from camera import VideoCamera
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder="static")
 
 cam = VideoCamera()
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html', sound_file_url='/static/sound/welcome_sound.wav')
+    return render_template(
+        "index.html", sound_file_url="/static/sound/welcome_sound.wav"
+    )
 
-@app.route('/fitness', methods=['GET', 'POST'])
+
+@app.route("/fitness", methods=["GET", "POST"])
 def fitness():
-    return render_template('camera.html')
+    return render_template("camera.html")
 
-@app.route('/video_feed')
+
+@app.route("/video_feed")
 def video_feed():
-    return Response(cam.gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(cam.gen(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
-# asdasdasdasdadsadadasdafsogfn9adiofnioa
