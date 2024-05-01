0ccf383567f1fbdad158b03c597a0a3ada5ee600
itzdeenzxx <fruitdf123@gmail.com>
2024-05-01 15:53:50 +0700

    fix detect hand

--- class_game/cam_counter.py
+++ class_game/cam_counter.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import mediapipe as mp
 import time
+from PIL import ImageFont, ImageDraw, Image 
 from class_game.gamereal import HandTracker
 import random as rd
 
@@ -19,30 +20,87 @@ class VideoCamera_Game(object):
         self.video = cv2.VideoCapture(self.camera_index)
         self.game = HandTracker()
         self.Number_random = [0] + [rd.randint(1, 5) for _ in range(4)]
-
-        
-        
-   
-            
-        
+        self.i = 0
 
     def __del__(self):
         self.video.release()
+
+    def draw_text(self, image, text, position):
+        if image is None:
+            return None
+        
+        pil_im = Image.fromarray(image) 
+        draw = ImageDraw.Draw(pil_im)
+        font = ImageFont.truetype("class_game/Prompt-Regular.ttf", 50)  
+        draw.text(position, text, font=font)  
+        cv2_im_processed = np.array(pil_im)
+        return cv2_im_processed
+
+    def show_number(self, frame, Number_random):
+        if frame is None:
+            return None
+        
+        text = ""
+        for idx, num in enumerate(Number_random):
+            if idx == self.i and self.i > 0:
+                text += "- "
+            elif idx < self.i:
+                text += f"{num} "
+            else:
+                text += "- "
+
+        if self.i < len(Number_random):
+            processed_frame = self.draw_text(frame, text.strip(), (50, 50))
+            if processed_frame is not None:
+                return processed_frame
+            else:
+                return frame
+        else:
+            return frame
+
+    def show_number_allrd(self, frame, Number_random):
+        if frame is None:
+            return None
+        
+        # คำนวณความกว้างและความสูงของภาพ
+        # ขนาดของข้อความ
+        text = f"ทำมือเลข {str(Number_random[0])} เพื่อเริ่มเกม"
+        text_num = f"จงจำเลข :{str(Number_random[1])},{str(Number_random[2])},{str(Number_random[3])},{str(Number_random[4])}"
+        text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_PLAIN, 3, 3)
+        # คำนวณตำแหน่งที่จะวางข้อความให้อยู่ตรงกล
+        if self.i <= 0:
+            processed_frame = self.draw_text(frame, text, (250, 300))
+            if processed_frame is not None:
+                processed_frame = self.draw_text(processed_frame, text_num, (265, 250))
+                return processed_frame
+            else:
+                return frame
+        else:
+            return frame
 
     def get_frame(self):
         ret, frame = self.video.read()
 
         if not ret:
+            print("Error: Failed to retrieve frame")
             return None
         
-        self.game.start_tracking(frame, self.Number_random) 
-        self.game.show_number(frame, self.Number_random)
-        self.game.show_number_allrd(frame, self.Number_random)
+        if frame is None:
+            print("Error: Received None frame")
+            return None
+
+        self.i = self.game.start_tracking(frame, self.Number_random) 
+        frame = self.show_number(frame, self.Number_random)
+        frame = self.show_number_allrd(frame, self.Number_random)
+
         ret, jpeg = cv2.imencode(".jpg", frame)
 
         if not ret:
+            print("Error: Failed to encode frame as JPEG")
             return None
+
         return jpeg.tobytes()
+
 
     def gen(self):
         while True:
--- class_game/gamereal.py
+++ class_game/gamereal.py
@@ -12,68 +12,61 @@ class HandTracker:
         self.hands = self.mp_hands.Hands()
         self.pTime = 0
         self.i= 0
-        
-        
+    
     def start_tracking(self,frame,Number_random):
-
         results = self.hands.process(frame)
-
         if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
+                hand_landmarks = results.multi_hand_landmarks[0]
                 for id, lm in enumerate(hand_landmarks.landmark):
-                    fingers_1 = 0
-                    fingers_2 = 0
-                    fingers_3 = 0
-                    fingers_4 = 0
-                    fingers_5 = 0
+                    fingers = 0
                     if id == 0:
                         continue
 
                     if (
-                        hand_landmarks.landmark[8].y < hand_landmarks.landmark[7].y
-                        and hand_landmarks.landmark[8].x > hand_landmarks.landmark[17].x
-                        or hand_landmarks.landmark[8].y < hand_landmarks.landmark[7].y
-                        and hand_landmarks.landmark[0].x > hand_landmarks.landmark[8].x
+                        hand_landmarks.landmark[8].y < hand_landmarks.landmark[5].y
+                        and hand_landmarks.landmark[4].y > hand_landmarks.landmark[5].y
+                        and hand_landmarks.landmark[12].y > hand_landmarks.landmark[7].y
+                        and hand_landmarks.landmark[16].y > hand_landmarks.landmark[7].y
+                        and hand_landmarks.landmark[20].y > hand_landmarks.landmark[7].y
                     ):
-                        fingers_1 += 1
+                        fingers = 1
+                    if (
+                        hand_landmarks.landmark[8].y < hand_landmarks.landmark[5].y
+                        and hand_landmarks.landmark[12].y < hand_landmarks.landmark[9].y
+                        and hand_landmarks.landmark[4].y > hand_landmarks.landmark[5].y
+                        and hand_landmarks.landmark[16].y > hand_landmarks.landmark[7].y
+                        and hand_landmarks.landmark[20].y > hand_landmarks.landmark[7].y
+                        
+                    ):
+                        fingers = 2
 
                     if (
-                        hand_landmarks.landmark[12].y < hand_landmarks.landmark[11].y
-                        and hand_landmarks.landmark[12].x
-                        > hand_landmarks.landmark[17].x
-                        or hand_landmarks.landmark[12].y < hand_landmarks.landmark[11].y
-                        and hand_landmarks.landmark[0].x > hand_landmarks.landmark[12].x
+                        hand_landmarks.landmark[8].y < hand_landmarks.landmark[5].y
+                        and hand_landmarks.landmark[12].y < hand_landmarks.landmark[9].y
+                        and hand_landmarks.landmark[16].y < hand_landmarks.landmark[13].y
+                        and hand_landmarks.landmark[4].y > hand_landmarks.landmark[5].y
+                        and hand_landmarks.landmark[20].y > hand_landmarks.landmark[17].y
                     ):
-                        fingers_2 += 1
+                        fingers = 3
 
-                    if (
-                        hand_landmarks.landmark[16].y < hand_landmarks.landmark[15].y
-                        and hand_landmarks.landmark[16].x
-                        > hand_landmarks.landmark[17].x
-                        or hand_landmarks.landmark[16].y < hand_landmarks.landmark[15].y
-                        and hand_landmarks.landmark[0].x > hand_landmarks.landmark[16].x
+                    if (hand_landmarks.landmark[8].y < hand_landmarks.landmark[5].y
+                        and hand_landmarks.landmark[12].y < hand_landmarks.landmark[9].y
+                        and hand_landmarks.landmark[16].y < hand_landmarks.landmark[13].y  
+                        and hand_landmarks.landmark[20].y < hand_landmarks.landmark[17].y   
                     ):
-                        fingers_3 += 1
+                        fingers = 4
 
-                    if (
-                        hand_landmarks.landmark[20].y < hand_landmarks.landmark[19].y
-                        and hand_landmarks.landmark[20].x
-                        > hand_landmarks.landmark[17].x
-                        or hand_landmarks.landmark[20].y < hand_landmarks.landmark[19].y
-                        and hand_landmarks.landmark[0].x > hand_landmarks.landmark[20].x
+                    if (hand_landmarks.landmark[8].y < hand_landmarks.landmark[5].y
+                        and hand_landmarks.landmark[12].y < hand_landmarks.landmark[9].y
+                        and hand_landmarks.landmark[16].y < hand_landmarks.landmark[13].y  
+                        and hand_landmarks.landmark[20].y < hand_landmarks.landmark[17].y  
+                        and hand_landmarks.landmark[4].y < hand_landmarks.landmark[5].y
                     ):
-                        fingers_4 += 1
-
-                    if hand_landmarks.landmark[4].x > hand_landmarks.landmark[2].x:
-                        fingers_5 += 1
+                        fingers = 5
                     if self.i < 5 :
                         if Number_random[self.i] == 0:
                             if(
-                                fingers_1 == 0
-                                and fingers_2 == 0
-                                and fingers_3 == 0
-                                and fingers_4 == 0
-                                and fingers_5 == 0
+                                fingers == 0
                             ):
                                 cv2.putText(
                                     frame,
@@ -87,11 +80,7 @@ class HandTracker:
                                 self.i += 1
                         if Number_random[self.i] == 1:
                             if(
-                                fingers_1 == 1
-                                and fingers_2 == 0
-                                and fingers_3 == 0
-                                and fingers_4 == 0
-                                and fingers_5 == 0
+                                fingers == 1
                             ):
                                 cv2.putText(
                                     frame,
@@ -103,13 +92,9 @@ class HandTracker:
                                     3,
                                 )
                                 self.i += 1
-                        elif Number_random[self.i] == 2:
+                        if Number_random[self.i] == 2:
                             if (
-                                fingers_1 == 1
-                                and fingers_2 == 1
-                                and fingers_3 == 0
-                                and fingers_4 == 0
-                                and fingers_5 == 0
+                                fingers == 2
                             ):
                                 cv2.putText(
                                     frame,
@@ -121,13 +106,9 @@ class HandTracker:
                                     3,
                                 )
                                 self.i += 1
-                        elif Number_random[self.i] == 3:
+                        if Number_random[self.i] == 3:
                             if (
-                                fingers_1 == 1
-                                and fingers_2 == 1
-                                and fingers_3 == 1
-                                and fingers_4 == 0
-                                and fingers_5 == 0
+                                fingers == 3
                             ):
                                 cv2.putText(
                                     frame,
@@ -139,13 +120,9 @@ class HandTracker:
                                     3,
                                 )
                                 self.i += 1
-                        elif Number_random[self.i] == 4:
+                        if Number_random[self.i] == 4:
                             if (
-                                fingers_1 == 1
-                                and fingers_2 == 1
-                                and fingers_3 == 1
-                                and fingers_4 == 1
-                                and fingers_5 == 0
+                                fingers == 4
                             ):
                                 cv2.putText(
                                     frame,
@@ -157,13 +134,9 @@ class HandTracker:
                                     3,
                                 )
                                 self.i += 1
-                        elif Number_random[self.i] == 5:
+                        if Number_random[self.i] == 5:
                             if (
-                                fingers_1 == 1
-                                and fingers_2 == 1
-                                and fingers_3 == 1
-                                and fingers_4 == 1
-                                and fingers_5 == 1
+                                fingers == 5
                             ):
                                 cv2.putText(
                                     frame,
@@ -175,72 +148,16 @@ class HandTracker:
                                     3,
                                 )
                                 self.i += 1
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                self.mp_drawing.draw_landmarks(frame, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
 
-    def show_number(self, frame, Number_random):
-        text = ""
-        for idx, num in enumerate(Number_random):
-            if idx == self.i and self.i > 0:
-                text += "- "
-            elif idx < self.i:
-                text += f"{num} "
-            else:
-                text += "- "
-
-        if self.i < len(Number_random):
-            cv2.putText(
-                frame,
-                text.strip() ,  
-                (50, 50),  
-                cv2.FONT_HERSHEY_PLAIN,
-                2,
-                (255, 0, 0),
-                2
-            )
+        return self.i
+    
         
 
 
-    def show_number_allrd(self, frame, Number_random):
-        # คำนวณความกว้างและความสูงของภาพ
-        height, width, _ = frame.shape
-        
-        # ขนาดของข้อความ
-        text = f"handful to start {str(Number_random[0])} "
-        text1 = f"Number:{str(Number_random[1])},{str(Number_random[2])},{str(Number_random[3])},{str(Number_random[4])}"
-        text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_PLAIN, 3, 3)
-        
-        
-        # คำนวณตำแหน่งที่จะวางข้อความให้อยู่ตรงกลาง
-        text_x = int((width - text_size[0]) / 2)
-        text_y = int((height + text_size[1]) / 2)
-        
-        if self.i <= 0 :
-            cv2.putText(
-                frame,
-                text,
-                
-                (250, 300),
-                cv2.FONT_HERSHEY_PLAIN,
-                2,
-                (255, 0, 0),
-                2,
-            )
-            cv2.putText(
-                frame,
-                text1,
-                
-                (265, 250),
-                cv2.FONT_HERSHEY_PLAIN,
-                2,
-                (255, 0, 0),
-                2,
-            )
+    
         
 
 
-
-    def stop_tracking(self):
-        self.cap.release()
-        cv2.destroyAllWindows()
-
-
-# Instantiate the HandTracker class and start tracking
